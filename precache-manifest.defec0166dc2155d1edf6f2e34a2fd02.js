82f78405f65c57be0b0e36371953ffe6398d83c9
Monisha <somanimonisha8@gmail.com>
2023-02-28 18:55:00 -0800

    Updates

---
+++ precache-manifest.defec0166dc2155d1edf6f2e34a2fd02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72b3be5c272af03d60f2",
+    "url": "/weather-forecast1/static/css/main.01cc5bc0.chunk.css"
+  },
+  {
+    "revision": "72b3be5c272af03d60f2",
+    "url": "/weather-forecast1/static/js/main.72b3be5c.chunk.js"
+  },
+  {
+    "revision": "ec5ddaa9f6f0a593122a",
+    "url": "/weather-forecast1/static/js/1.ec5ddaa9.chunk.js"
+  },
+  {
+    "revision": "01fa298f9eccbaecfb0b",
+    "url": "/weather-forecast1/static/js/2.01fa298f.chunk.js"
+  },
+  {
+    "revision": "78cbf6d7cf43d5cbedbf",
+    "url": "/weather-forecast1/static/js/runtime~main.78cbf6d7.js"
+  },
+  {
+    "revision": "3726bdcf6ba2356ce061a6667059f53d",
+    "url": "/weather-forecast1/index.html"
+  }
+];
