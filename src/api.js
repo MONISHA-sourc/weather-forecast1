7399b295efd253f47188b6de955f258e39e580cd
Monisha <somanimonisha8@gmail.com>
2023-02-28 18:08:57 -0800

    deploying to GitHub Pages

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7949a41e8dmsh929909f507263cdp10dce8jsn402ca5a26068',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="bb23d190905506d8240bac142435e964";
